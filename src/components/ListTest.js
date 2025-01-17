274b169222a00baf58ac47bd0782dc64b717e27e
TânTri2003 <votantri2006@gmail.com>
2025-01-17 23:14:29 +0700

    tri up

--- src/components/ListTest.js
+++
@@ -1,7 +0,0 @@
-export default function ListTest(){
-    return(
-        <h1>
-            Hello world
-        </h1>
-    )
-}
---
+++ src/components/TriComponent/HomeTest.js
@@ -0,0 +1,5 @@
+export default function HomeTest (){
+    return(
+        <h1>Home</h1>
+    )
+}
