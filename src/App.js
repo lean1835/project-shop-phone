8c2e4aa5524bd25c97f67d11c5c5993d550f6572
phong244 <phong.levanthanh@gmail.com>
2025-01-18 21:29:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-
 import './App.css';
-import ListTest from './components/AnComponent/ListTest';
-import HomeTest from './components/TriComponent/HomeTest';
+
 function App() {
-  return (
-    <div className="App">
-          <ListTest/>
-          <HomeTest/>
-    </div>
-  );
+    return (
+        <div className="App">
+            hi
+        </div>
+    );
 }
 export default App;
