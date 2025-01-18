83a594da680ed45f04ed31ef1fbe64ed57786a2e
TrucLinhNguyen1805 <truclinhnguyen1805@gmail.com>
2025-01-18 17:08:56 +0700

    linh up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import ListTest from './components/AnComponent/ListTest';
+import TestList from './components/LinhComponent/TestList';
 import HomeTest from './components/TriComponent/HomeTest';
 function App() {
   return (
     <div className="App">
           <ListTest/>
           <HomeTest/>
+          <TestList/>
     </div>
   );
 }
