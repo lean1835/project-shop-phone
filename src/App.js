56632fee5b15f60ab54a7b7582e0fef767c0e809
TânTri2003 <votantri2006@gmail.com>
2025-01-23 18:50:13 +0700

    up login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
-
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from "react-router-dom"
+import LoginComponent from './components/LoginComponent/LoginComponent';
+import HeaderComponent from './components/HomeComponent/HeaderComponent';
+import { ToastContainer } from 'react-toastify';
 function App() {
     return (
-        <div className="App">
-            hi
-        </div>
+        <>
+        <ToastContainer/>
+            <Routes>
+                <Route path="/" element={<LoginComponent/>} />
+                <Route path="/home" element={<HeaderComponent />} />
+            </Routes>
+        </>
+
     );
 }
 export default App;
