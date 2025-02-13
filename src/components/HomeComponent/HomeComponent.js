34c112b8a14b1d46ee494a0aa01266ed77aa6d88
TânTri2003 <votantri2006@gmail.com>
2025-02-13 22:32:02 +0700

    up login

--- src/components/HomeComponent/HomeComponent.js
+++ src/components/HomeComponent/HomeComponent.js
@@ -3,11 +3,13 @@ import HeaderComponent from './HeaderComponent'
 import BannerComponent from './BannerComponent'
 import BodyComponent from './BodyComponent'
 import FooterComponent from './FooterComponent'
+import ManageComponent from './ManageComponent'
 
 function HomeComponent(){
     return(
         <>
         <HeaderComponent/>
+        <ManageComponent/>
         <BannerComponent/>
         <BodyComponent/>
         <FooterComponent/>
